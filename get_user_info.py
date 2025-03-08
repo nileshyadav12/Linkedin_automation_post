6befed43030895fa612fdc07cc0e9cca9ce9c3bd
nileshyadav12 <nileshyadavnill404@gmail.com>
2025-03-08 09:54:33 +0530

    get_user_info.py

---
+++ get_user_info.py
@@ -0,0 +1,19 @@
+import requests
+
+from Oauth import auth, headers
+
+def user_info(headers):
+    '''
+    Get user information from Linkedin
+    '''
+    response = requests.get('https://api.linkedin.com/v2/userinfo', headers = headers)
+    user_info = response.json()
+    return user_info
+
+if __name__ == '__main__':
+    credentials = rf'C:\Users\User\Desktop\deep\LinkedIn-Post-Automation-Python-ChatGPT\credentials.json'
+    access_token = auth(credentials) # Authenticate the API
+    headers = headers(access_token) # Make the headers to attach to the API call.
+    user_info = user_info(headers) # Get user info
+    print(user_info)
+
